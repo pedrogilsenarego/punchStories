d0631e94108557a8530d3a23dc11e76b52611cc9
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-06-28 12:11:55 +0100

    a

--- src/modules/Story/Tamplate1/index.tsx
+++ src/modules/Story/Tamplate1/index.tsx
@@ -1,19 +1,23 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { Template } from "../types";
 import { IoIosArrowBack } from "react-icons/io";
-import "./index.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { State } from "../../../slicer/types";
 import { Book } from "../../../slicer/books/books.types";
 import { useNavigate } from "react-router";
 import { ROUTE_PATHS } from "../../../constants/routes";
+import { generalConstants } from "../../../constants/general";
 
 const Template1 = ({ storyData }: Template) => {
   const textRef = useRef<HTMLDivElement>(null);
   const imgRef = useRef<HTMLImageElement>(new Image(150, 150));
   const story = useSelector<State, Book>((state) => state.books.book);
   const lang = useSelector<State, string>((state) => state.general.lang);
+  const [hover, setHover] = useState<boolean>(false);
+  const theme = useTheme();
+  const numberPictures = story?.content2.length;
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
   const navigate = useNavigate();
   useEffect(() => {
     const textElem = textRef.current;
@@ -31,20 +35,26 @@ const Template1 = ({ storyData }: Template) => {
       textElem.innerHTML = words.join(" ");
     }
   }, []);
+  const xsNumber = () => {
+    if (numberPictures === 1 || mobile) return 12;
+    if (numberPictures === 2) return 6;
+    if (numberPictures >= 3) return 4;
+  };
   return (
     <>
       <div
         style={{
           width: "100vw",
-          backgroundColor: "#bcc0be",
+          backgroundColor: "#ffffff",
           position: "relative",
-          minHeight: "100vh",
+
           display: "flex",
           justifyContent: "space-between",
         }}
       >
         <img
           style={{
+            position: "fixed",
             width: "40%",
             height: "100vh",
           }}
@@ -73,21 +83,21 @@ const Template1 = ({ storyData }: Template) => {
             style={{ marginLeft: "-5px" }}
           />
         </div>
-        <div
-          style={{
-            position: "absolute",
 
-            top: "2vw",
-            right: "2vw",
-            cursor: "pointer",
+        <div
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          style={{
+            position: "relative",
+            top: "20vh",
+            left: "35%",
+            height: "25vw",
             display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+
+            cursor: "pointer",
+            transition: "all 1.5s ease-in-out",
           }}
         >
-          <Typography style={{ fontWeight: 800 }}>04, May 2023</Typography>
-        </div>
-        <div style={{ position: "absolute", top: "20%", left: "35%" }}>
           <img
             src={story?.content2[0]}
             alt=''
@@ -95,39 +105,58 @@ const Template1 = ({ storyData }: Template) => {
               width: "25vw",
               height: "25vw",
               objectFit: "cover",
-              border: "solid 10px #bcc0be",
+
+              border: "solid 10px #ffffff",
+              transition: "all 1.5s ease-in-out",
             }}
           />
+
+          <div
+            style={{
+              position: "absolute",
+              left: "30px",
+              top: "20px",
+              marginTop: "10px",
+              backgroundColor: "whiteSmoke",
+              width: "calc(25vw - 20px)",
+              height: !hover ? "calc(25vw - 20px)" : "calc(27vw - 20px)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "40px",
+
+              opacity: hover ? 1 : 0,
+
+              transition: "all 1.5s ease-in-out",
+              boxShadow: "6px 6px 10px #00000039",
+            }}
+          >
+            <Typography
+              style={{
+                fontSize: "24px",
+                fontFamily: "spaceMono",
+                textAlign: "justify",
+              }}
+            >
+              "{story?.punchLines[0] || ""}"
+            </Typography>
+          </div>
         </div>
+
         <div
           style={{
-            position: "absolute",
-            top: "calc(20% + 30vw + 10px)",
-            left: "calc(40% + 10px)",
-            width: "calc(19.5% - 20px)",
-          }}
-        >
-          <Typography style={{ fontSize: "18px", fontFamily: "spaceMono" }}>
-            "{story?.punchLines[0] || ""}"
-          </Typography>
-        </div>
-        <div
-          style={{
-            height: "95vh",
-            width: "42.5%",
-            padding: "30px",
-            marginTop: "40px",
+            width: "100%",
+            paddingRight: "60px",
+            paddingTop: "40px",
+            marginTop: hover ? "71vh" : "67vh",
+            transition: "all 1.5s ease-in-out",
+            display: "flex",
+            justifyContent: "end",
           }}
         >
           <div
-            className='glass-background'
             style={{
-              height: "100%",
-              overflowY: "scroll",
-              padding: "15px 15px 15px 30px",
-              borderRadius: "10px",
-              paddingTop: "20px",
-              backgroundColor: "",
+              width: "75%",
             }}
           >
             <Typography
@@ -142,18 +171,53 @@ const Template1 = ({ storyData }: Template) => {
               #{story?.postNumber}_{story?.name},{story?.age}
             </Typography>
 
-            <Typography style={{ fontFamily: "spaceMono", marginTop: "14px" }}>
+            <Typography
+              style={{
+                fontFamily: "spaceMono",
+                marginTop: "14px",
+                textAlign: "justify",
+              }}
+            >
               {lang === "PT" ? story?.resume : story?.resumeEN}
             </Typography>
             <Typography
               style={{
                 fontFamily: "spaceMono",
-                marginTop: "20px",
-                marginBottom: "20px",
+                marginTop: "30px",
               }}
             >
               {lang === "PT" ? story?.ps : story?.psEN}
             </Typography>
+            <Grid
+              container
+              marginTop='40px'
+              marginBottom="40px"
+              columnSpacing='10px'
+              rowSpacing='10px'
+            >
+              {story?.content2?.map((image, index) => {
+                return (
+                  <Grid
+                    item
+                    xs={xsNumber()}
+                    key={index}
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <img
+                      alt=''
+                      src={image}
+                      height='500px'
+                      width='100%'
+                      style={{ objectFit: mobile ? "cover" : "contain" }}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
           </div>
         </div>
       </div>
